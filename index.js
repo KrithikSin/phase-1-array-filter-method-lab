5df64e62d0ccf46a383377667a5b664d788b40e3
Krithik Singh <krithiksingh@gmail.com>
2023-04-16 13:24:20 -0400

    arry filter method completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const findMatching = (names, str) => {
+  return names.filter((name) => name.toLowerCase() === str.toLowerCase());
+};
+
+const fuzzyMatch = (names, str) => {
+  return names.filter(function (name) {
+    if (name[0] === str[0]) {
+      return name;
+    }
+  });
+};
+
+const matchName = (names, str) => {
+  return names.filter((name) => name["name"] === str);
+};
